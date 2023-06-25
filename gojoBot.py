ee40f237f9c07ce600a25103d6c117ca41865ee4
Derpman <immapotato8110@gmail.com>
2023-06-25 17:36:45 -0400

    Update gojoBot.py

--- gojoBot.py
+++ gojoBot.py
@@ -1,189 +1,163 @@
-# Author: Guanyi Cao
-# Date: July 7, 2022
-# Based on tutorial by CS Dojo and modified for my own purposes.
-
 from queue import Empty
 import random
 import tweepy
 import time
 import schedule
 
-# twitter bot credentials
+# Twitter bot credentials
 CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
 CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 ACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXX'
 ACCESS_SECRET = 'XXXXXXXXXXXXXXXXXXXXX'
 
+# Authenticate with Twitter API
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth) 
+api = tweepy.API(auth)
 
-# constants
+# Constants
 FILE_NAME = 'last_seen_id.txt'
 REPLY_QUOTES = 'reply_quotes.txt'
 TWEET_QUOTES = 'tweet_quotes.txt'
 ARCHIVE = 'quote_archive.txt'
 DANCE_GIF = 'gojo_dance.gif'
-NUM_TWEETS = 5 # replies to 5 tweets for now
-CHARACTER_LIMIT = 221 # adjusted from 280 w/ addition of message: "Bestowing Gojo's.... You're welcome!" (59 chars)
+NUM_TWEETS = 5  # Replies to 5 tweets for now
+CHARACTER_LIMIT = 221  # Adjusted from 280 with the addition of message: "Bestowing Gojo's.... You're welcome!" (59 chars)
 
-recentQuotes = [] # keeps track of recent quotes used for replies
+recentQuotes = []  # Keeps track of recent quotes used for replies
 
-# read in quote file
-# only take in quotes short enough
+
+# Read quote file and return a list of quotes
 def read_quote_file(filename):
     quote_list = []
-    file1 = open(filename, 'r')
-    
-    while True:
-        # Get next line from file
-        line = file1.readline().strip()
-        # ensure it is short enough for a tweet
-        if (len(line) <= CHARACTER_LIMIT):
-            quote_list.append(line)
-        else:
-            print(line) # prints quotes that are too long
-        
-        # if line is empty
-        # end of file is reached
-        if not line:
-            break
-    
-    file1.close()
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if len(line) <= CHARACTER_LIMIT:
+                quote_list.append(line)
+            else:
+                print(line)  # Print quotes that are too long
     quote_list.remove('')
     return quote_list
 
 
-# retrieves a random quote from list of quotes,
-# ensures new quote hasn't been retrieved recently
+# Get a random quote from the quote list, ensuring it hasn't been used recently
 def get_random_quote(quote_list, isTweet):
     if not isTweet:
-        if recentQuotes is Empty:
-            quote = random.choice(quote_list)
-        else:
+        if recentQuotes:
             while True:
                 quote = random.choice(quote_list)
-                if recentQuotes.count(quote) == 0:
+                if quote not in recentQuotes:
                     break
+        else:
+            quote = random.choice(quote_list)
     else:
         quote = random.choice(quote_list)
-    
+
     used_quotes(quote, quote_list, isTweet)
     return quote
 
 
-# if a reply, adds retrieved quote to recent quotes list
-# if a tweet, adds retrieved quote into archive file, 
-# and removes it from tweet quotes file
+# If a reply, add the retrieved quote to the recent quotes list.
+# If a tweet, add the retrieved quote to the archive file and remove it from the tweet quotes file.
 def used_quotes(quote, quote_list, isTweet):
     if isTweet:
-        # adds used quote into quote archive file
-        f = open(ARCHIVE, 'a')
-        f.write(quote + '\n')
-        f.close()
-        
-        # removes from tweet quotes list
-        # rewrites tweet quotes file
+        # Add used quote to the quote archive file
+        with open(ARCHIVE, 'a') as f:
+            f.write(quote + '\n')
+
+        # Remove the quote from the tweet quotes list and rewrite the tweet quotes file
         quote_list.remove(quote)
-        f = open(TWEET_QUOTES, 'w')
-        for line in quote_list:
-            f.write(line + '\n')
-        f.close()
-    
+        with open(TWEET_QUOTES, 'w') as f:
+            f.write('\n'.join(quote_list))
+
     else:
         recentQuotes.append(quote)
-        # clears recent quotes once all quotes have been used and starts over
+        # Clear recent quotes once all quotes have been used and start over
         if len(recentQuotes) == len(quote_list):
             recentQuotes.clear()
 
 
-# reads id from file and returns it
+# Read the last seen id from the file
 def retrieve_last_seen_id(file_name):
-    f_read = open(file_name, 'r')
-    last_seen_id = int(f_read.read().strip())
-    f_read.close()
+    with open(file_name, 'r') as f:
+        last_seen_id = int(f.read().strip())
     return last_seen_id
 
-# stores last seen id back in file
-def store_last_seen_id(last_seen_id, file_name):
-    f_write = open(file_name, 'w')
-    f_write.write(str(last_seen_id))
-    f_write.close()
-    return
 
-# retrieves <NUM_TWEETS> recent tweets with the hashtag 'GojoSatoru' or 'gojosatoru'
-# replies to each tweet w/ randomly chosen quote from quotes list
+# Store the last seen id back in the file
+def store_last_seen_id(last_seen_id, file_name):
+    with open(file_name, 'w') as f:
+        f.write(str(last_seen_id))
+
+
+# Search for recent tweets with the hashtag 'GojoSatoru' or 'gojosatoru' and reply to each tweet
 def search_and_reply(quote):
-    # retrieves tweets
-    tweets = tweepy.Cursor(api.search_tweets, q='#GojoSatoru -filter:retweets' 
-    or '#gojosatoru -filter:retweets', lang='en', tweet_mode='extended').items(NUM_TWEETS)
-    
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    tweets = tweepy.Cursor(api.search_tweets, q='#GojoSatoru -filter:retweets OR #gojosatoru -filter:retweets',
+                           lang='en', tweet_mode='extended').items(NUM_TWEETS)
+
     tweet_list = [tweet for tweet in tweets]
-    
+
     for tweet in reversed(tweet_list):
-        # ensures bot doesn't reply twice by storing last seen id
         last_seen_id = tweet.id
         store_last_seen_id(last_seen_id, FILE_NAME)
-        
-        print('tweet id: ' +str(tweet.id))
-        print('username: ' +tweet.user.screen_name)
-        print('text: '+tweet.full_text)
-        
-        # replies to tweet with a two minute delay
+
+        print('Tweet ID: ' + str(tweet.id))
+        print('Username: ' + tweet.user.screen_name)
+        print('Text: ' + tweet.full_text)
+
+        # Reply to the tweet with a two-minute delay
         time.sleep(120)
-        api.update_status('@' + tweet.user.screen_name 
-        + ' bestowing Gojo\'s wisdom with a daily quote:\n' +quote, in_reply_to_status_id=tweet.id)
+        api.update_status('@' + tweet.user.screen_name +
+                          ' bestowing Gojo\'s wisdom with a daily quote:\n' + quote, in_reply_to_status_id=tweet.id)
 
 
-# tweets randomly chosen quote of the day
+# Tweet a randomly chosen quote of the day
 def tweet(quote):
-    api.update_status('Gojo Senseii\'s Quote of the Week:\n' + quote)
+    api.update_status('Gojo Sensei\'s Quote of the Week:\n' + quote)
 
-# replies with dancing gojo GIF if mentioned
+
+# Reply with a dancing Gojo GIF if mentioned
 def gojo_dance():
-    print('looking for dance commands...')
-    # use tweet id 1543426093255987200 for testing.
+    print('Looking for dance commands...')
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
-    
     mentions = api.mentions_timeline(since_id=last_seen_id, tweet_mode='extended')
-    
+
     for mention in reversed(mentions):
         print(str(mention.id) + ' - ' + mention.full_text)
         last_seen_id = mention.id
         store_last_seen_id(last_seen_id, FILE_NAME)
         if 'dance' in mention.full_text.lower():
-            print('found dance command!')
-            print('gojo will dance now')
+            print('Found dance command!')
+            print('Gojo will dance now')
             api.update_status_with_media('@' + mention.user.screen_name, DANCE_GIF, in_reply_to_status_id=mention.id)
 
 
-# EXECUTING
-# -----------------------------------------
-# performs all actions associated with search and reply
+# Scheduled job to search and reply to tweets
 def reply_job():
-    # get quotes for replies, retrieves random quote
-    replyQuotes = read_quote_file(REPLY_QUOTES)
-    quoteForReply = get_random_quote(replyQuotes, False)
+    reply_quotes = read_quote_file(REPLY_QUOTES)
+    quote_for_reply = get_random_quote(reply_quotes, False)
+    search_and_reply(quote_for_reply)
 
-    # searches for relevant tweets and replies to each
-    search_and_reply(quoteForReply) 
 
+# Schedule the reply job to run daily at 12:00:00
 schedule.every().day.at('12:00:00').do(reply_job)
 
 
-# performs all actions associated with tweets on main page
+# Scheduled job to tweet a quote
 def tweet_job():
-    # get quotes for tweets, retrieves random quote
-    tweetQuotes = read_quote_file(TWEET_QUOTES)
-    quoteToTweet = get_random_quote(tweetQuotes, True)
-    tweet(quoteToTweet)
+    tweet_quotes = read_quote_file(TWEET_QUOTES)
+    quote_to_tweet = get_random_quote(tweet_quotes, True)
+    tweet(quote_to_tweet)
 
+
+# Schedule the tweet job to run weekly
 schedule.every().week.do(tweet_job)
 
 
-# execute bot actions
+# Execute bot actions
 while True:
-    schedule.run_pending() # run scheduled tasks
-
+    schedule.run_pending()  # Run scheduled tasks
     gojo_dance()
     time.sleep(15)
